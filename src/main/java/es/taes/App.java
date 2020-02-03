3c3162fb7ed91523a891d14085ed0208e07e6d2f
Agueda <aguedafontestadcubells@gmail.com>
2020-02-03 13:31:06 +0100

    inicial 2

---
+++ src/main/java/es/taes/App.java
@@ -0,0 +1,17 @@
+package es.taes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Persona persona = new Persona();
+        persona.setEdad(32);
+        persona.setNombre ("Agueda");
+    System.out.println(persona);
+    }
+}
